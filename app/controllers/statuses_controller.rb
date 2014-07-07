2388dfc869302b4f11f63035a1e29e8b6b64cc5e
live-alone <rohitkhatana.khatana@gmail.com>
2014-07-07 15:53:43 +0530

    added individual profile page

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,75 +1,71 @@
 class StatusesController < ApplicationController
-  before_filter :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_status, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:new, :create, :update, :destroy, :edit]
 
-  # GET /statuses
-  # GET /statuses.json
   def index
     @statuses = Status.all
   end
 
-  # GET /statuses/1
-  # GET /statuses/1.json
   def show
   end
 
-  # GET /statuses/new
   def new
     @status = Status.new
   end
 
-  # GET /statuses/1/edit
   def edit
+    if @status.user != current_user
+      redirect_to root_path, alert: "You can't edit this status, it ain't yours!"
+    end
   end
 
-  # POST /statuses
-  # POST /statuses.json
   def create
-    @status = Status.new(status_params)
-
+    @status = current_user.statuses.new status_params
     respond_to do |format|
       if @status.save
-        format.html { redirect_to @status, notice: 'Status was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @status }
+        format.html { redirect_to @status, notice: "Status was successfully created." }
+        format.js
       else
-        format.html { render action: 'new' }
-        format.json { render json: @status.errors, status: :unprocessable_entity }
+        render action: 'new'
       end
     end
   end
 
-  # PATCH/PUT /statuses/1
-  # PATCH/PUT /statuses/1.json
   def update
-    respond_to do |format|
-      if @status.update(status_params)
-        format.html { redirect_to @status, notice: 'Status was successfully updated.' }
-        format.json { head :no_content }
+    if @status.user == current_user
+      if @status.update status_params
+        redirect_to @status, notice: "Status was successfully updated."
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @status.errors, status: :unprocessable_entity }
+        render action: 'edit'
       end
+    else
+      redirect_to root_path, alert: "You attempted to update a status that is not yours"
     end
   end
 
-  # DELETE /statuses/1
-  # DELETE /statuses/1.json
   def destroy
-    @status.destroy
-    respond_to do |format|
-      format.html { redirect_to statuses_url }
-      format.json { head :no_content }
+    if @status.user == current_user
+      @status.destroy
+      respond_to do |format|
+        format.html { redirect_to statuses_url }
+        format.js
+      end
+    else
+      redirect_to root_path
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_status
-      @status = Status.find(params[:id])
+      @status = Status.find params[:id]
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The status you are looking for cannot be found."
+      redirect_to root_path
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def status_params
-      params.require(:status).permit(:content, :user_id)
+      params.require(:status).permit(:content)
     end
 end
